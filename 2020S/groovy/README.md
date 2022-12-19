69b2b89a5e4ba1f1bad06e2967ae8134cf95de4c
Mark Sherman <shermanm@emmanuel.edu>
2022-12-19 14:49:29 -0500

    Groovy
    Co-authored-by: brennanf <64998306+brennanf@users.noreply.github.com>
    Co-authored-by: Jaclyn Kennedy <kennedyj3@emmanuel.edu>

---
+++ 2020S/groovy/README.md
@@ -0,0 +1,12 @@
+# Groovy
+
+  Hello! Welcome to the grooviest final to return to the era of the flower power. The program plays simply by clicking run and is complete with a classic 70s tune by the Bee Gees, a far out background, and a sunflower just enjoying life wherever the wind blows it. Can you dig it?
+
+  In our project, we were able to create a moving background with funky music and an image of a flower that bounces randomly off the sides of the canvas. The only task we did not complete was adding sound effects to the flower when the bounced off the side as it felt unnecessary and like a sensory overload.
+
+  Jaclyn completed the code for the moving background while Faith configured the music along with the flower image and its bouncing abilities.
+  
+  Keep On Keepin' On,
+  Faith Brennan & Jaclyn Kennedy
+  
+https://brennanf.github.io/Groovy/
---
+++ 2020S/groovy/sketch.js
@@ -0,0 +1,85 @@
+/* 
+Final Assignment
+Faith Brennan & Jaclyn Kennedy
+*/
+
+//background
+var y = 100;
+var x = 100;
+var m = -1;
+var n = -1;
+
+//sunflower
+var a = 0;
+var b = 200;
+var speedA = 3;
+var speedB = 4;
+var angle = 0;
+
+//sound
+var mySound;
+
+//image
+let img;
+
+function preload() {
+  soundFormats('mp3');
+  mySound = loadSound('stayinalive.mp3');
+  img = loadImage('sunflower.png');
+}
+
+function setup() {
+  mySound.play();
+  createCanvas(windowWidth, windowHeight);
+  stroke('orange');
+  strokeWeight(25);
+  frameRate(150);
+}
+
+function draw() {
+  background('lightblue');
+
+  y = y + (3 * m);
+  if (y < 0) {
+    m = m * (-1);
+  }
+  if (y > height) {
+    m = m * (-1);
+  }
+  fill('lightYellow');
+  ellipse(0, y, width / 2);
+  ellipse(width, y, width / 2);
+
+  ellipse(0, y, width / 2);
+  ellipse(width, y, width / 2);
+
+  ellipse(0, y, width / 2);
+  ellipse(width, y, width / 2);
+
+  x = x + (3 * n);
+  if (x < 0) {
+    n = n * (-1);
+  }
+  if (x > width) {
+    n = n * (-1);
+  }
+  ellipse(0, height / 2, x);
+  ellipse(0, height / 2, x);
+
+  ellipse(width / 2, height / 2, x);
+  ellipse(width / 2, height / 2, x);
+
+  ellipse(width, height / 2, x);
+  ellipse(width, height / 2, x);
+
+  image(img, a, b, 120, 120);
+  a += speedA;
+  b += speedB;
+
+  if (a > width || a < 0) {
+    speedA *= -1;
+  }
+  if (b > height || b < 0) {
+    speedB *= -1;
+  }
+}
