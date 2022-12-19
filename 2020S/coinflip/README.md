eb3982bfc8df315874090d235e0543def079c006
Mark Sherman <shermanm@emmanuel.edu>
2022-12-19 14:55:02 -0500

    Coin Flip
    Co-authored-by: corletoj <64984668+corletoj@users.noreply.github.com>

---
+++ 2020S/coinflip/README.md
@@ -0,0 +1,19 @@
+# Coin-Flip
+
+## Introduction
+A Coin flip heads/tails simulator written in Javascript utilizing the P5 3D library. 
+We were able to successfully create a cylinder with the faces of a quarter in a 3D canvas that flips at the press of the spacebar, have the program randomly select heads or tails, and implement a sound whenever the spacebar is pressed.
+
+## Instructions
+Press the spacebar to start the program, and use the text boxes labeled heads or tails to keep track of the score.
+
+## Credits
+
+#### Original concept - Jonathan Torrez-Lopez
+
+#### 3D animation - Jonathan Torrez-Lopez & Joseph Corleto
+
+#### Texture and Sound Implementation - Jonathan Torrez-Lopez & Joseph Corleto
+
+
+https://corletoj.github.io/Coin-Flip/
---
+++ 2020S/coinflip/sketch.js
@@ -0,0 +1,86 @@
+let flip = false;
+let land = false;
+let begin = 0;
+const flipHeight = -300;
+const flipTime = 3000; // in milliseconds
+let headsImg;
+let tailsImg;
+let flipSound;
+let options = ['heads', 'tails'];
+
+function preload() {
+  headsImg = loadImage('quarterheads.png');
+  tailsImg = loadImage('quarter-tails-MA.png');
+  font = loadFont('RobotoSlab-Regular.ttf');
+  flipSound = loadSound('CoinFlipSound.wav');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight - 100, WEBGL);
+  noStroke();
+}
+
+function draw() {
+  ambientLight('white');
+  background('beige');
+  translate(0, 150, 0);
+
+  const time = millis() - begin;
+
+  if (flip) {
+    translate(0, sin(time * (PI / flipTime)) * flipHeight, 0);
+    rotateX(time / 500);
+    rotateZ(time / 75);
+    rotateY(time / 1000);
+    land = false;
+  }
+
+  if (keyIsPressed && key === ' ') {
+    flip = true;
+    land = false;
+    begin = millis();
+    flipSound.play();
+    shuffle(options, true);
+  }
+
+  if (land) {
+    textFont(font);
+    textSize(32);
+    fill('black');
+    text(options[0], -50, -250);
+  }
+
+  if (time > flipTime && begin != 0) {
+    flip = false;
+    land = true;
+  }
+  //rotate(PI);
+  coin();
+}
+
+ function coin() {
+  specularMaterial('silver');
+  cylinder(50, 4.8);
+
+  if (land && options[0] == 'heads') {
+     noStroke();
+     rotateX(HALF_PI);
+     translate(0, 0, 2.5);
+     texture(headsImg);
+     plane(100, 100);
+
+     translate(0, 0, -5);
+     texture(tailsImg);
+     plane(100, 100);
+  } else {
+      noStroke();
+      rotateX(HALF_PI);
+      translate(0, 0, 2.5);
+      texture(tailsImg);
+      plane(100, 100);
+
+      translate(0, 0, -5);
+      texture(headsImg);
+      plane(100, 100);
+  }
+}
